bf8826df9e5c1836b0a206e7a3604069610d1091
Harsh Singh <128421323+h4rshgithub@users.noreply.github.com>
2023-10-09 14:34:17 +0530

    For college project

---
+++ main.js
@@ -0,0 +1,50 @@
+
+function openForm() {
+  document.getElementById("myForm").style.display = "flex";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+
+function oO1(){
+  document.getElementById("o1").style.display = "flex";
+}
+
+function oC1(){
+ document.getElementById("o1").style.display = "none";
+}
+
+function oO2(){
+  document.getElementById("o2").style.display = "flex";
+}
+
+function oC2(){
+ document.getElementById("o2").style.display = "none";
+}
+
+function oO3(){
+  document.getElementById("o3").style.display = "flex";
+}
+
+function oC3(){
+ document.getElementById("o3").style.display = "none";
+}
+
+function oO4(){
+  document.getElementById("o4").style.display = "flex";
+}
+
+function oC4(){
+ document.getElementById("o4").style.display = "none";
+}
+
+
+function trainB(){
+  document.getElementById("bookTrain").style.display = "flex";
+}
+
+function trainC(){
+  document.getElementById("bookTrain").style.display = "none";
+}
